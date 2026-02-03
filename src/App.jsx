e8d483222cc64c4524be1d2c0ccba50e6f0ce404
Bryan Gordon <bryanbge92@gmail.com>
2026-02-02 21:33:32 -0500

    Add main page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 // import { useState } from 'react'
-import './App.css'
+import Footer from './components/Footer'
+import './styles/App.css'
 
 function App() {
   return (
-    <h1>
-      Page
-    </h1>
+    <>
+    <header>
+      <h1>
+        Ticket Generator
+      </h1>
+    </header>
+    <div>
+      <h5>Generate any kind of ticket everywhere</h5>
+    </div>
+    <article>
+      <div>
+        <h2>
+          Choose one kind of ticket
+        </h2>
+        
+      </div>
+    </article>
+    <Footer />
+    </>
   )
 }
 
