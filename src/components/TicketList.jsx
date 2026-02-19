33a7a14e8f01245686f65af28bc2b9583f9d0df0
Bryan Gordon <bryanbge92@gmail.com>
2026-02-18 19:53:38 -0500

    Add component for generate each ticket

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,17 @@
+import "../styles/TicketList.css"
+
+export function TicketList () {
+  return (
+    <ul>
+      <li>
+        <a href="#">Concerts</a>
+      </li>
+      <li>
+        <a href="#">Movies</a>
+      </li>
+      <li>
+        <a href="#">Trains</a>
+      </li>
+    </ul>
+  )
+}
