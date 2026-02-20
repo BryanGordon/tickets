22e4fad8e48eb9174bc5d7069e00afe712828a4b
Bryan Gordon <bryanbge92@gmail.com>
2026-02-19 20:05:29 -0500

    Refactor component

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -2,16 +2,6 @@ import "../styles/TicketList.css"
 
 export function TicketList () {
   return (
-    <ul>
-      <li>
-        <a href="#">Concerts</a>
-      </li>
-      <li>
-        <a href="#">Movies</a>
-      </li>
-      <li>
-        <a href="#">Trains</a>
-      </li>
-    </ul>
+  <></>   
   )
 }
