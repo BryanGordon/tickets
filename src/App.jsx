4796aa22dc5328da5361d90409983f3ed3fdb557
Bryan Gordon <bryanbge92@gmail.com>
2026-02-26 20:56:30 -0500

    Update component tag structure
    - Wrap content with main tag

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import './styles/App.css'
 function App() {
   return (
     <>
+    <main>
     <header>
       <h1>
         Ticket Generator
@@ -28,6 +29,7 @@ function App() {
       </div>
     </article>
     <TicketInfo />
+    </main>
     <Footer />
     </>
   )
