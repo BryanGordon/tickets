b0a3b72255ec6d841991a71bc3268513efacaaa5
Bryan Gordon <bryanbge92@gmail.com>
2026-03-03 19:49:28 -0500

    Update component
    - Delete unused component
    - Delete title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // import { useState } from 'react'
 import Footer from './components/Footer'
 import { TicketInfo } from './components/TicketInfo'
-import { TicketList } from './components/TicketList'
 import './styles/App.css'
 
 function App() {
@@ -13,19 +12,11 @@ function App() {
         Ticket Generator
       </h1>
     </header>
-    <div>
-      <h5>Generate any kind of ticket everywhere</h5>
-    </div>
     <article>
       <div>
         <h2>
           Choose one kind of ticket
         </h2>
-        {/*
-        <div className='container-tickets'>
-          <TicketList />
-        </div>
-        */}
       </div>
     </article>
     <TicketInfo />
