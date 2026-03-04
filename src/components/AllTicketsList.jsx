d7f3b7ef231cdffda9386318bfb17c8a4e6d91d0
Bryan Gordon <bryanbge92@gmail.com>
2026-03-04 18:50:17 -0500

    Create component structure

--- src/components/AllTicketsList.jsx
+++ src/components/AllTicketsList.jsx
@@ -1,6 +1,33 @@
+import { NavLink } from 'react-router-dom'
+import '../styles/AllTicketsList.css'
+
 export function AllTicketsList () {
   return (
-    <>
-    </>
+    <section>
+      <header>
+      <div className='back-container'>
+        <NavLink to='/'>Go back</NavLink>
+      </div>
+      </header>
+      <article>
+        <div className='ticket-data-card'>
+          <span>Ticket type: Train</span>
+          <span>Number: 313132</span>
+          <span className='validated'>Validated</span>
+        </div>
+
+        <div className='ticket-data-card'>
+          <span>Ticket type: Train</span>
+          <span>Number: 313132</span>
+          <span className='validated'>Validated</span>
+        </div>
+
+        <div className='ticket-data-card'>
+          <span>Ticket type: Train</span>
+          <span>Number: 313132</span>
+          <span className='validated'>Validated</span>
+        </div>
+      </article>
+    </section>
   )
 }
