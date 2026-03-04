19cca9b5eeef8d74129cdb12625bff21a845185a
Bryan Gordon <bryanbge92@gmail.com>
2026-03-04 18:49:52 -0500

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 // import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import { TicketInfo } from './components/TicketInfo'
 import './styles/App.css'
+import { AllTicketsList } from './components/AllTicketsList'
+import { Generator } from './components/Generator'
 
 function App() {
   return (
     <>
     <main>
+    <Router>
+    <Routes>
+      <Route path='/' element={<Generator />}/>
+      <Route path='/tickets-list' element={<AllTicketsList />}/>
+    </Routes>
+    </Router>
     </main>
     <Footer />
     </>
