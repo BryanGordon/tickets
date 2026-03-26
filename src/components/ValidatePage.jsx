079418eb0a3f9dbdf81080722a93db52f2404a55
Bryan Gordon <bryanbge92@gmail.com>
2026-03-25 20:46:28 -0500

    Add logic for validation page

--- src/components/ValidatePage.jsx
+++ src/components/ValidatePage.jsx
@@ -1,8 +1,38 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+
 export function ValidatePage () {
-  return (
-    <>
-      <h2>Ticket validado correctamente.</h2>
-      <h2>El ticket no se ha podido validar.</h2>
+  const [validate, setValidate] = useState(false)
+  const { id } = useParams()
+
+  useEffect(() => {
+    const getValidation = async () => {
+      const res = await fetch(`http://192.168.0.178:3000/validation/${id}`, {
+        method: "PATCH"
+      })
+
+      if (res.ok) {
+        setValidate(true)
+      }
+    }
+
+    getValidation()
+    console.log(id)
+    
+  }, [])
+
+  if (validate) {
+    return (
+      <>
+      {
+        console.log(id)
+      }
+    <h2>Ticket validado correctamente.</h2>
     </>
   )
+  }
+
+  else return <h2>El ticket no se ha podido validar.</h2> 
+  
 }
