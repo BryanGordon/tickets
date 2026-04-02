76ffeb0d44ceeeac6b6c71bd18eb265481f23075
Bryan Gordon <bryanbge92@gmail.com>
2026-04-01 22:04:27 -0500

    Delete comments and format code

--- src/components/AllTicketsList.jsx
+++ src/components/AllTicketsList.jsx
@@ -1,8 +1,9 @@
-import { NavLink } from 'react-router-dom'
 import '../styles/AllTicketsList.css'
-import { NoItemsList } from './NoItemsList'
+
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
+import { NoItemsList } from './NoItemsList'
 
 export function AllTicketsList () {
   const [tickets, setTickets] = useState([])
@@ -31,76 +32,41 @@ export function AllTicketsList () {
 
   if (tickets.length === 0) {
     return (
-    <>
-    <header>
-      <div className='back-container'>
-        <NavLink to='/'>Go back</NavLink>
-      </div>
-      </header>
-    <NoItemsList />
+      <>
+        <header>
+          <div className='back-container'>
+            <NavLink to='/'>Go back</NavLink>
+          </div>
+        </header>
+        <NoItemsList />
     </>
     )
   }
 
   else {
     return (
-      <>
-        <section>
-      <header>
-      <div className='back-container'>
-        <NavLink to='/'>Go back</NavLink>
-      </div>
-      </header>
-      <article>
-        {
-          tickets.map((ticket) => (
-            <div className='ticket-data-card' key={ticket.number}>
-          <div className='info-container'>
-            <span>Ticket type: {ticket.type}</span>
-            <span>Number: {ticket.number}</span>
+      <section>
+        <header>
+          <div className='back-container'>
+            <NavLink to='/'>Go back</NavLink>
           </div>
-          <div>
-            <span className={ticket.validated ? "validated" : "not-validated"}>{ticket.validated ? 'Validated' : 'Not validated'}</span>  
-          </div>
-        </div>
-          )
-          ) 
-
-        }
-        {/** 
-        <div className='ticket-data-card'>
-          <div className='info-container'>
-            <span>Ticket type: Train</span>
-            <span>Number: 313132</span>
-          </div>
-          <div>
-            <span className='validated'>Validated</span>  
-          </div>
-        </div>
-
-        <div className='ticket-data-card'>
-          <div className='info-container'>
-            <span>Ticket type: Train</span>
-            <span>Number: 313132</span>
-          </div>
-          <div>
-            <span className='validated'>Validated</span>
-          </div>
-        </div>
-
-        <div className='ticket-data-card'>
-          <div className='info-container'>
-            <span>Ticket type: Train</span>
-            <span>Number: 313132</span>
-          </div>
-          <div>
-            <span className='validated'>Validated</span>
-          </div>
-        </div>
-        */}
-      </article>
-    </section>
-      </>
+        </header>
+        <article>
+          {
+            tickets.map((ticket) => (
+              <div className='ticket-data-card' key={ticket.number}>
+                <div className='info-container'>
+                  <span>Ticket type: {ticket.type}</span>
+                  <span>Number: {ticket.number}</span>
+                </div>
+                <div>
+                  <span className={ticket.validated ? "validated" : "not-validated"}>{ticket.validated ? 'Validated' : 'Not validated'}</span>  
+                </div>
+              </div>
+            ))
+          }
+        </article>
+      </section>
     )
   }
 }
