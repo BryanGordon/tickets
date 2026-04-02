0fb467c746872572b60090115ada4f89370e5cf4
Bryan Gordon <bryanbge92@gmail.com>
2026-04-01 22:04:58 -0500

    Format code

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -4,18 +4,14 @@ export function Generator () {
   return (
     <section>
       <header>
-      <h1>
-        Ticket Generator
-      </h1>
-    </header>
-    <article>
-      <div>
-        <h2>
-          Choose one kind of ticket
-        </h2>
-      </div>
-    </article>
-    <TicketInfo />
+        <h1>Ticket Generator</h1>
+      </header>
+      <article>
+        <div>
+          <h2>Choose one kind of ticket to generate</h2>
+        </div>
+      </article>
+      <TicketInfo />
     </section>
   )
 }
